eacc902d4613744837d31f5145ab6561db6d024f
Tarun <negitarun9899@gmail.com>
2021-03-25 17:45:21 +0530

    Adding all files

---
+++ main.js
@@ -0,0 +1,7 @@
+const burger = document.getElementsByClassName('burger')[0];
+const nav = document.getElementsByClassName('nav-links')[0];
+
+
+burger.addEventListener('click', ()=>{
+    nav.classList.toggle('nav-active');
+})
